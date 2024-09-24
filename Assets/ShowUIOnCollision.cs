386eb8bd72dbf04c418ceffd25a6d9436a0f5671
CharmStellar <charmijodhpura73200@gmail.com>
2024-09-24 01:32:04 -0400

    Final

--- Assets/ShowUIOnCollision.cs
+++ Assets/ShowUIOnCollision.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShowUIOnCollision : MonoBehaviour
 {
     // Reference to the UI element you want to show/hide
     public GameObject uiElement;
-
+    public GameObject Player;
     // Called when something enters the trigger collider
     private void OnTriggerEnter(Collider other)
     {
         // Check if the player entered the trigger collider
-        if (other.CompareTag("Player"))
+        if (other.gameObject == Player)
         {
             // Enable the UI element when the player enters the collision box
             uiElement.SetActive(true);
+            Debug.Log("entered");
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject == Player)
         {
             // Enable the UI element when the player enters the collision box
             uiElement.SetActive(true);
@@ -30,7 +32,7 @@ public class ShowUIOnCollision : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         // Check if the player exited the trigger collider
-        if (other.CompareTag("Player"))
+        if (other.gameObject == Player)
         {
             // Disable the UI element when the player exits the collision box
             uiElement.SetActive(false);
